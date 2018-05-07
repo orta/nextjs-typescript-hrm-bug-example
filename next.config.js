95631144d9957b209a4811a4d152af76978eaea9
Orta <orta.therox@gmail.com>
2018-05-07 08:59:59 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -30,12 +30,12 @@ const currentExport = {
 
     // This does work:
     //
-    if (dev && !isServer) {
-      const HMR = config.module.rules.find(r => r.loader === "hot-self-accept-loader")
-      if (HMR) {
-        HMR.test = /\.(ts|tsx)$/
-        HMR.options.extensions = /\.(ts|tsx)$/
-      }
+    // if (dev && !isServer) {
+    //   const HMR = config.module.rules.find(r => r.loader === "hot-self-accept-loader")
+    //   if (HMR) {
+    //     HMR.test = /\.(ts|tsx)$/
+    //    HMR.options.extensions = /\.(ts|tsx)$/
+    //  }
     }
 
     return config
